47fed3bf0f6ab7b1d36be2073269675e667ea051
anamarisortiz <anamarisortiz@hotmail.com>
2024-07-13 03:16:07 -0400

    recipe-result-appen

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,19 +1,12 @@
 const searchBtn = document.getElementById("button");
-const containerRecipeResults = document.getElementById("container-recipe-results");
+const recipeResults = document.getElementById("recipe-results");
 const searchApi = `https://api.spoonacular./recipes/complexSearch/?`
 const apiKey = "5fa9f5e50dce47b9aa6ec8cd7538f0c8"
 const containerVideoResults = document.getElementById("container-video-results");
 const formInput = document.getElementById("findtext");
 const getUrl = document.getElementById("get-url");
 
-function getParams() {
-    
-    const searchParamsArr = document.location.search.split('&');
-    const query = searchParamsArr[0].split('=').pop();
-    const format = searchParamsArr[1].split('=').pop();
-  
-    searchApi(query, format);
-  }
+const DESCRIPTION_LENGTH = 100; //Characters
 
 function searchrecipeApi(query) {
   return `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${query}&addRecipeInstructions=${query}&number=5`
@@ -21,83 +14,125 @@ function searchrecipeApi(query) {
 function recipeIdInfo(id) {
   return `https://api.spoonacular.com/recipes/${id}/information?apiKey=${apiKey}`
 }
-     
 
-  searchBtn.addEventListener('click', function (event) {
-    event.preventDefault();
-    const q = formInput.value;
-    const apiUrl = searchrecipeApi(q);
-    
+function buildRecipeCardThumbnail(result) {
+  // building recipe thumbnail
+  const recipeThumbnail = document.createElement("div");
+  recipeThumbnail.setAttribute("class", "recipe-card-thumbnail");
+  const recipeLink = document.createElement("a")
+  const recipeCardImage = document.createElement("img");
 
-    fetch(apiUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-           console.log(data)
-        // getRecipe(data)
-        for (let i =0; i < data.results.length; i++) {
-          const resultTitle = document.createElement("h3");
-            const resultDescription = document.createElement("p");
-            const resultImg = document.createElement("img");
-            const resultButton = document.createElement("button");
-            resultButton.textContent = "view recipe";
-            const itemID = data.results[i].id;
-            resultButton.setAttribute("id", itemID);
-            resultButton.addEventListener('click', function() {
-                const recipeInfoURL = recipeIdInfo(itemID);
-                fetch(recipeInfoURL)
-                    .then((response) =>  {
-                        return response.json()
-                    })
-                    .then((response) => {
-                      const recipeURL = response.sourceUrl;
-                      window.open(recipeURL, "_blank");
-                      console.log({ response });
-                    });
-            });
-            
-          
-            resultTitle.textContent = "" + data.results[i].title;
-            resultImg.setAttribute("src", data.results[i].image);
+  // setting attributes
+  recipeCardImage.setAttribute('src', result.image);
+  recipeLink.setAttribute("href", result.image);
 
-            containerRecipeResults.append(resultTitle, resultDescription, resultImg, resultButton);
-        }
-      })
-    });
+  // appending nodes to parent
+  recipeThumbnail.append(recipeLink);
+  recipeLink.append(recipeCardImage);
 
-    
-const baseURL2 = `https://www.googleapis.com/youtube/v3/search`
-const apiKey2 = "AIzaSyB0XEvk44MkvgqFCfBpU2eGm60Nc7hXh6c"
+  return recipeThumbnail;
+}
+
+function buildRecipeCardInfo(result, description, prop1) {
+  // Building recipe card info 
+  const recipeCardInfo = document.createElement("div");
+  const recipeCardTitle = document.createElement("h5");
+  const recipeCardDesc = document.createElement("span");
+  const recipeCardProp1 = document.createElement("span");
+
+  //setting attributes
+  recipeCardInfo.setAttribute("class", "recipe-card-info");
+  recipeCardTitle.setAttribute("class", "recipe-card-title");
+  recipeCardDesc.setAttribute("class", "recipe-card-desc");
+  recipeCardProp1.setAttribute('class', 'recipe-card-prop');
 
 
+  recipeCardDesc.textContent = description.slice(0, DESCRIPTION_LENGTH) + ' ...'; // format desc so its right length
+  recipeCardProp1.textContent = `Ready in ${prop1} minutes.`;
+  recipeCardTitle.textContent = result.title;
 
-  function searchYoutubeApi (query) {
-return `${baseURL2}?part=snippet&maxResults=5&q=${encodeURIComponent(query)}&key=${apiKey2}`
-  }
+  // appending nodes
+  recipeCardInfo.append(recipeCardTitle);
+  recipeCardInfo.append(recipeCardDesc);
+  recipeCardInfo.append(recipeCardProp1);
+
+  return recipeCardInfo;
+}
+
+function buildRecipeCardButton(result, sourceUrl) {
+  const recipeCardButton = document.createElement('div');
+  recipeCardButton.setAttribute('class', 'recipe-card-button');
+
+  const recipeButton = document.createElement('button');
+  recipeButton.setAttribute('type', 'submit');
+  recipeButton.setAttribute('id', 'get-url');
+  recipeButton.textContent = 'View Recipe';
+
+  const itemID = result.id;
+  // create onClick listener
+  recipeButton.addEventListener('click', function () {
+    window.open(sourceUrl, '_blank');
+  });
+
+  recipeCardButton.append(recipeButton);
+  return recipeCardButton;
+}
+
+function buildRecipeCardNode(result, srcURL, description, prop1) {
+  const recipeCard = document.createElement('div');
+  recipeCard.setAttribute('class', 'recipe-card');
+
+  const thumbnail = buildRecipeCardThumbnail(result);
+
+  const info = buildRecipeCardInfo(result, description, prop1);
+
+  const button = buildRecipeCardButton(result, srcURL);
+
+  recipeCard.append(thumbnail);
+
+  recipeCard.append(info);
+
+  recipeCard.append(button);
+
+  return recipeCard;
+}
 
 
-  searchBtn.addEventListener('click', function (event) {
+searchBtn.addEventListener('click', function (event) {
+  event.preventDefault();
+  const q = formInput.value;
+  const apiUrl = searchrecipeApi(q);
 
-    event.preventDefault();
-    const q2 = formInput.value;
-    const apiUrl2 = searchYoutubeApi(q2);
-    
 
-    fetch(apiUrl2)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-
-        });
+  fetch(apiUrl)
+    .then(function (response) {
+      return response.json();
     })
+    .then(function (data) {
 
-    
+      // clear the recipe results container
+      recipeResults.innerHTML = '';
+
+      for (const recipe of data.results) {
+
+        const recipeInfoURL = recipeIdInfo(recipe.id);
+
+        // need to make two api calles for summary, desc and target url 
+        fetch(recipeInfoURL)
+          .then((response) => {
+            return response.json()
+          })
+          .then((response) => {
+            const recipeCard = buildRecipeCardNode(recipe, response.sourceUrl, response.summary, response.readyInMinutes);
+            recipeResults.append(recipeCard);
+          });
+      }
+    })
+});
 
 
-    
 
 
-            
+
+
+
