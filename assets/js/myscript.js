2b78f08afdef5bc9e88ac5b153e859daa40f58c9
egodschalk <egodschalk@gmail.com>
2024-07-13 16:21:33 -0400

    bugs

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -6,27 +6,16 @@ const containerVideoResults = document.getElementById("container-video-results")
 const formInput = document.getElementById("findtext");
 const getUrl = document.getElementById("get-url");
 
-<<<<<<< HEAD
 const DESCRIPTION_LENGTH = 100; //Characters
-=======
-function getParams() {
 
-  const searchParamsArr = document.location.search.split('&');
-  const query = searchParamsArr[0].split('=').pop();
-  const format = searchParamsArr[1].split('=').pop();
 
-  searchApi(query, format);
-}
->>>>>>> dev
-
-function searchrecipeApi(query) {
+function searchRecipeApi(query) {
   return `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${query}&addRecipeInstructions=${query}&number=5`
 }
 function recipeIdInfo(id) {
   return `https://api.spoonacular.com/recipes/${id}/information?apiKey=${apiKey}`
 }
 
-<<<<<<< HEAD
 function buildRecipeCardThumbnail(result) {
   // building recipe thumbnail
   const recipeThumbnail = document.createElement("div");
@@ -90,10 +79,11 @@ function buildRecipeCardButton(result, sourceUrl) {
   return recipeCardButton;
 }
 
+
 function buildRecipeCardNode(result, srcURL, description, prop1) {
   const recipeCard = document.createElement('div');
   recipeCard.setAttribute('class', 'recipe-card');
-
+  
   const thumbnail = buildRecipeCardThumbnail(result);
 
   const info = buildRecipeCardInfo(result, description, prop1);
@@ -113,54 +103,45 @@ function buildRecipeCardNode(result, srcURL, description, prop1) {
 searchBtn.addEventListener('click', function (event) {
   event.preventDefault();
   const q = formInput.value;
-  const apiUrl = searchrecipeApi(q);
+  const apiUrl = searchRecipeApi(q);
 
-
-  fetch(apiUrl)
-=======
-
-searchBtn.addEventListener('click', function (event) {
-  event.preventDefault();
-  const q = formInput.value;
-  const apiUrl = searchrecipeApi(q);
-
+  // const recipeResultsTitle = document.createElement("h3");
+  // recipeResultsTitle.textContent = 'Recipe Search Results';
+  // recipeResultsTitle.classList.add("recipe-results-container-title")
+  // recipeCard.append(recipeResultsTitle);
 
   fetch(apiUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data)
-      // getRecipe(data)
-      for (let i = 0; i < data.results.length; i++) {
-        const resultTitle = document.createElement("h3");
-        const resultDescription = document.createElement("p");
-        const resultImg = document.createElement("img");
-        const resultButton = document.createElement("button");
-        resultButton.textContent = "view recipe";
-        const itemID = data.results[i].id;
-        resultButton.setAttribute("id", itemID);
-        resultButton.addEventListener('click', function () {
-          const recipeInfoURL = recipeIdInfo(itemID);
-          fetch(recipeInfoURL)
-            .then((response) => {
-              return response.json()
-            })
-            .then((response) => {
-              const recipeURL = response.sourceUrl;
-              window.open(recipeURL, "_blank");
-              console.log({ response });
-            });
-        });
+      console.log(data);
+      // clear the recipe results container
+      recipeResults.innerHTML = '';
 
+      for (const recipe of data.results) {
 
-        resultTitle.textContent = "" + data.results[i].title;
-        resultImg.setAttribute("src", data.results[i].image);
+        const recipeInfoURL = recipeIdInfo(recipe.id);
 
-        containerRecipeResults.append(resultTitle, resultDescription, resultImg, resultButton);
+        // need to make two api calls for summary, desc and target url 
+        fetch(recipeInfoURL)
+          .then((response) => {
+            console.log(recipeInfoURL)
+            return response.json()
+          })
+          .then((response) => {
+            const recipeCard = buildRecipeCardNode(recipe, response.spoonacularSourceUrl, response.summary, response.readyInMinutes);
+            recipeResults.append(recipeCard);
+          });
       }
     })
 });
+
+
+
+
+
+
 
 
 const baseURL2 = `https://www.googleapis.com/youtube/v3/search`
@@ -173,73 +154,44 @@ searchBtn.addEventListener('click', function (event) {
 
   event.preventDefault();
 
-  const q2 = formInput.value;
+  const q2 = formInput.value + " food network cooking video";
   const apiUrl2 = `${baseURL2}?part=snippet&maxResults=5&q=${q2}&key=${apiKey2}`
-
+  const videoCardEl = document.createElement("div");
+  videoCardEl.classList.add("video-card");
+  containerVideoResults.append(videoCardEl);
+  // const videoResultsTitle = document.createElement("h3");
+  // videoResultsTitle.textContent = 'Cooking Videos';
+  // videoResultsTitle.classList.add("video-results-container-title")
+  // videoCardEl.append(videoResultsTitle);
 
   fetch(apiUrl2)
->>>>>>> dev
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-<<<<<<< HEAD
-
-      // clear the recipe results container
-      recipeResults.innerHTML = '';
-
-      for (const recipe of data.results) {
-
-        const recipeInfoURL = recipeIdInfo(recipe.id);
-
-        // need to make two api calles for summary, desc and target url 
-        fetch(recipeInfoURL)
-          .then((response) => {
-            return response.json()
-          })
-          .then((response) => {
-            const recipeCard = buildRecipeCardNode(recipe, response.sourceUrl, response.summary, response.readyInMinutes);
-            recipeResults.append(recipeCard);
-          });
-      }
-    })
-});
-
-
-
-=======
       console.log(data);
       for (let i = 0; i < data.items.length; i++) {
-
-        const videoCardEl = document.createElement("div");
-        videoCardEl.classList.add("video-card");
-        containerVideoResults.append(videoCardEl);
->>>>>>> dev
 
         const videoImgEl = document.createElement("img");
         videoImgEl.classList.add("video-card-thumbnail");
         videoImgEl.src = data.items[i].snippet.thumbnails.medium.url;
         videoCardEl.append(videoImgEl);
 
-<<<<<<< HEAD
-=======
         const videoTitleEl = document.createElement("a");
         videoTitleEl.classList.add("video-card-title");
         videoTitleEl.textContent = "" + data.items[i].snippet.title;
         videoTitleEl.href = `https://www.youtube.com/watch?v=${data.items[i].id.videoId}`;
         videoCardEl.append(videoTitleEl);
->>>>>>> dev
 
-        const videoDescEl = document.createElement("a");
+        const videoDescEl = document.createElement("p");
         videoDescEl.classList.add("video-card-desc");
-        videoDescEl.textContent = "" + data.items[i].snippet.description;
+        videoDescEl.textContent = "" + data.items[i].snippet.description.slice(0, DESCRIPTION_LENGTH) + ' ...';
         videoCardEl.append(videoDescEl);
 
-<<<<<<< HEAD
-=======
+        formInput.value = '';
+
       }
 
     });
 
 });
->>>>>>> dev
